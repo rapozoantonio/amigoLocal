7ac1ba1e8d602c6ecfee6df6fe1aee32ef31788f
Antonio Rapozo <antonio.rapozo@mayvue.com>
2025-01-30 17:46:44 +0100

    fix routing issue with promoter-page

--- src/router/index.js
+++ src/router/index.js
@@ -125,7 +125,7 @@ const routes = [
                 /* webpackChunkName: "promoter-info" */ "@/views/promoters/PromoterInfo.vue"
               ),
             meta: {
-              isActive: false,
+              isActive: true,
             },
           },
           {
